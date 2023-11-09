25b2e76fab18528d3c0a8e5ea0668d77de3d9748
Thiagodfarias <thiago__07@hotmail.com>
2023-11-09 15:51:09 -0400

    primeiro commit

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+const saudacaoParaAlguem = saudacao("Betina");
+console.log(saudacaoParaAlguem);
